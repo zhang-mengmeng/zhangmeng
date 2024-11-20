df9c496dcc39aa5d8200e45714d2ffa015b6e091
zhang-mengmeng <1245274264@qq.com>
2024-11-20 14:39:02 +0800

    zm

---
+++ server/app.js
@@ -0,0 +1,106 @@
+import express from 'express';
+import fs from 'node:fs';
+const app = express();
+
+app.use('*',(req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin',"*");
+    res.setHeader('Access-Control-Allow-Headers',"content-type");
+    res.setHeader('Access-Control-Allow-Methods','DELETE,PATCH,POST,GET,PUT,OPTIONS')
+    next()
+})
+
+app.use(express.json())
+
+// 获取数据列表
+app.get('/list',(req,res)=>{
+    fs.readFile('server/data/data.json', {
+        encoding: 'utf8',
+        flag: 'r'
+    },(err, data)=>{
+        if(JSON.parse(data).length == 0) {
+            res.json({
+                code: 200,
+                msg: '暂无数据',
+                data: []
+            })
+        }else {
+            res.json({
+                code: 200,
+                msg: '获取成功',
+                data: JSON.parse(data)
+            })
+        }
+    })
+})
+
+// 删除数据
+app.delete('/del',(req,res) =>{
+    fs.readFile('server/data/data.json', {
+        encoding: 'utf8',
+        flag:'r+'
+    },(err,data) =>{
+        let dataa = JSON.parse(data)
+        dataa.forEach((item,index) =>{
+            if(item.username == req.body.username) {
+                dataa.splice(index,1)
+            }
+        })
+        fs.writeFileSync('server/data/data.json',JSON.stringify(dataa),(err) =>{
+            if(!err) {
+                res.json({
+                    code:200,
+                    msg:'删除成功'
+                })
+            }else {
+                res.json({
+                    code:500,
+                    msg:'删除失败'
+                })
+            }
+        })
+    })
+})
+
+// 新增数据
+app.post('/add',(req,res) =>{
+    fs.readFile('server/data/data.json',{
+        encoding: 'utf8',
+        flag:'r+'
+    },(err,data) =>{
+        if(err) return err
+        if(JSON.parse(data).length == 0) {
+            fs.writeFile('server/data/data.json',JSON.stringify([req.body]),(err)=>{
+
+            })
+        }else {
+            fs.writeFile('server/data/data.json',JSON.stringify([...JSON.parse(data),req.body]),(err,data) =>{
+                if(!err) {
+                    res.json({
+                        code:200,
+                        msg:'新增成功'
+                    })
+                }
+            })
+        }
+    })
+})
+
+// 查询数据
+app.post('/search',(req,res) =>{
+    fs.readFile('server/data/data.json',{
+        encoding: 'utf8',
+        flag:'r+'
+    },(err, data) =>{
+        let filteredArray = JSON.parse(data).filter(item => {
+            return req.body.username ? item.username.includes(req.body.username) : true;
+        })
+        res.json({
+            code:200,
+            msg:'查询成功'
+        })
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('服务启动成功')
+})
